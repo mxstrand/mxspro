c8601956b6dd850cd0c16cf809e97b8ee4725fc5
Mike Strand <mxstrand@hotmail.com>
2013-06-22 19:16:21 -0700

    prod test

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,5 +4,5 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-#Mxspro::Application.config.secret_token = 'a0d4f72473bb51b94450aa9bd4fc6885b8472a59db4bfe50dce6aed60978788c477a08898af6070f37e65f650a6eb15e5f72f2ee213443e2aacebd371dd7fa8c'
-Mxspro::Application.config.secret_token = ENV["SECRET_TOKEN"]
+Mxspro::Application.config.secret_token = 'a0d4f72473bb51b94450aa9bd4fc6885b8472a59db4bfe50dce6aed60978788c477a08898af6070f37e65f650a6eb15e5f72f2ee213443e2aacebd371dd7fa8c'
+# Mxspro::Application.config.secret_token = ENV["SECRET_TOKEN"]
