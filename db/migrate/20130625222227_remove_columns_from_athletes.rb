9f69f259e17ea34ab129b18ee01203b4ef09e9d3
Mike Strand <mxstrand@hotmail.com>
2013-06-25 17:25:07 -0700

    fixed spelling error in migrating puking at Heroku

--- db/migrate/20130625222227_remove_columns_from_athletes.rb
+++ db/migrate/20130625222227_remove_columns_from_athletes.rb
@@ -1,11 +1,9 @@
 class RemoveColumnsFromAthletes < ActiveRecord::Migration
   def up
     remove_column :athletes, :athletenews
-    remove_column :athletes, :athleteprofie
   end
 
   def down
-    add_column :athletes, :athleteprofie, :string
     add_column :athletes, :athletenews, :string
   end
 end
